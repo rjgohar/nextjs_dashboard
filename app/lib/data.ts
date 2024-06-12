63335b2368120bab1ec8caf02da4e3b5560e9847
rjgohar <rehmatjan143@gmail.com>
2024-06-12 16:16:27 +0500

    add cards and invoices

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -9,6 +9,7 @@ import {
   Revenue,
 } from './definitions';
 import { formatCurrency } from './utils';
+import { Epilogue } from 'next/font/google';
 
 export async function fetchRevenue() {
   // Add noStore() here to prevent the response from being cached.
